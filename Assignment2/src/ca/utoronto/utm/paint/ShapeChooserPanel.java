9798141d5db8078c2bf9b079f6c1c58a5b98f421
Manaal Arshad <Manaal@Manaals-MacBook-Air.local>
2018-11-01 19:33:39 -0400

    bug 2 completed

--- Assignment2/src/ca/utoronto/utm/paint/ShapeChooserPanel.java
+++ Assignment2/src/ca/utoronto/utm/paint/ShapeChooserPanel.java
@@ -2,8 +2,10 @@ package ca.utoronto.utm.paint;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
+
 
 public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEvent> {
 
@@ -12,22 +14,34 @@ public class ShapeChooserPanel extends GridPane implements EventHandler<ActionEv
 	public ShapeChooserPanel(View view) {
 
 		this.view = view;
+		
+	
+		
+		
 
 		String[] buttonLabels = { "Circle", "Rectangle", "Square", "Squiggle", "Polyline" };
 
 		int row = 0;
+		ToggleGroup toggleGroup = new ToggleGroup();
 		for (String label : buttonLabels) {
-			Button button = new Button(label);
+			ToggleButton button = new ToggleButton(label);		
 			button.setMinWidth(100);
 			this.add(button, 0, row);
 			row++;
 			button.setOnAction(this);
+			
+			button.setToggleGroup(toggleGroup);
 		}
+		
+		toggleGroup.getSelectedToggle();
+		
+		
+		
 	}
 
 	@Override
 	public void handle(ActionEvent event) {
-		String command = ((Button) event.getSource()).getText();
+		String command = ((ToggleButton) event.getSource()).getText();
 		this.view.getPaintPanel().setMode(command);
 		System.out.println(command);
 	}
