b26f2c5676c5548edc1eaba426059147c42a2c7f
Manahil <manahil.masroor@outlook.com>
2018-11-13 12:16:22 -0500

    Updated PaintModel - Manahil
    Conflicts:
    	Assignment2/scrum/dailyScrumMeeting2.txt
    	Assignment2/scrum/productBacklog.txt
    	Assignment2/scrum/sprintBacklog.txt
    	Assignment2/src/ca/utoronto/utm/paint/Circle.java
    	Assignment2/src/ca/utoronto/utm/paint/ColourChooserPanel.java
    	Assignment2/src/ca/utoronto/utm/paint/PaintModel.java
    	Assignment2/src/ca/utoronto/utm/paint/PaintPanel.java
    	Assignment2/src/ca/utoronto/utm/paint/Rectangle.java
    	Assignment2/src/ca/utoronto/utm/paint/ShapeChooserPanel.java
    	Assignment2/src/ca/utoronto/utm/paint/View.java

--- Assignment2/src/ca/utoronto/utm/paint/PaintModel.java
+++ Assignment2/src/ca/utoronto/utm/paint/PaintModel.java
@@ -1,13 +1,8 @@
 package ca.utoronto.utm.paint;
 
-
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Stack;
-
-
-
-
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
@@ -20,13 +15,15 @@ public class PaintModel extends Observable {
 	private ArrayList<Circle> circles = new ArrayList<Circle>();
 	private ArrayList<Rectangle> rectangles = new ArrayList<Rectangle>();
 	private ArrayList<Polyline> polylines = new ArrayList<Polyline>();
-	
+
 	private ArrayList<Double> pointsW = new ArrayList<Double>();
 	private ArrayList<Double> circlesW = new ArrayList<Double>();
 	private ArrayList<Double> rectanglesW = new ArrayList<Double>();
 	private ArrayList<Double> polylinesW = new ArrayList<Double>();
 	
 	private Stack<Shapes> shapeStack = new Stack<Shapes>();
+	private Stack <Stack<Shapes>> deletedShapes = new Stack <Stack<Shapes>>();
+	
 	
 	
 	
@@ -44,8 +41,47 @@ public class PaintModel extends Observable {
 		shape.setStrokeWidth(TCP.getLineWidth());
 		this.shapeStack.push(shape);
 		System.out.println("shape added to stack:" + this.shapeStack);
-		modelChanged();	
+		modelChanged();
+		
 	}
+	
+	/**
+	 * Removes the last drawn shape shape from the canvas
+	 */
+	
+	public void undo() {
+
+		if(this.shapeStack.size() > 0) {
+			Stack<Shapes> shapeStack = new Stack<Shapes>();
+			shapeStack.push(this.shapeStack.pop());
+			this.deletedShapes.push(shapeStack);
+			
+		}
+		
+	}
+	
+	/**
+	 * Adds the shape back to the canvas that was removed using undo option
+	 */
+	
+	public void redo () {
+		if(this.deletedShapes.size()>0) {
+			Stack<Shapes> shapeStack = this.deletedShapes.pop();
+			while(shapeStack.size() != 0) {
+				this.shapeStack.push(shapeStack.pop());
+			}				
+		}
+		
+	}
+	
+	public Stack<Shapes> getShapeStack() {
+		return this.shapeStack;
+	}
+	
+	public Stack<Stack<Shapes>> getDeletedShapes(){
+		return this.deletedShapes;
+	}
+	
 	public void modelChanged() {
 		this.setChanged();
 		this.notifyObservers();
@@ -75,7 +111,6 @@ public class PaintModel extends Observable {
 	public ArrayList<Double> getPointsW() {
 		return pointsW;
 	}
-	//
 	
 	public void addCircle(Circle c) {
 		this.circles.add(c);
@@ -83,7 +118,6 @@ public class PaintModel extends Observable {
 		this.setChanged();
 		this.notifyObservers();
 	}
-	
 
 
 	public ArrayList<Circle> getCircles() {
